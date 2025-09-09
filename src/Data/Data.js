63c231c0506c46e018843a770e31307a8d468985
anadecs <anadecs@gmail.com>
2025-09-09 23:15:13 +0800

    Initial commit

---
+++ src/Data/Data.js
@@ -0,0 +1,44 @@
+export const sample = [
+  {
+    id: 1,
+    img: "src/assets/html.png",
+    title: "html",
+    description:
+      "The backbone of web pages. Defines the structure and content of websites using elements and tags.",
+  },
+  {
+    id: 2,
+    img: "src/assets/github.png",
+    title: "Github",
+    description:
+      "Helps developers track changes in code, collaborate with others, and manage project history efficiently",
+  },
+  {
+    id: 3,
+    img: "src/assets/css.png",
+    title: "CSS",
+    description:
+      "Used to style and design web pages — controls colors, layouts, fonts, and responsiveness.",
+  },
+  {
+    id: 4,
+    img: "src/assets/react.png",
+    title: "React",
+    description:
+      "A powerful UI library for building fast, reusable, and interactive components for modern web apps.",
+  },
+  {
+    id: 5,
+    img: "src/assets/java.png",
+    title: "Java",
+    description:
+      "A versatile, platform-independent programming language widely used in enterprise, Android, and backend systems.",
+  },
+  {
+    id: 6,
+    img: "src/assets/js.png",
+    title: "JavaScript",
+    description:
+      "Web Programming Language  The scripting language of the web that brings interactivity, logic, and dynamic features to websites.",
+  },
+];
